53f5bd3b6f092ea04d1eb450c8c1fec64c2050fd
Lakr1m0za <mohax8romeo@gmail.com>
2021-04-05 19:07:11 +0300

    Add scripts from work

---
+++ python_scripts/example.py
@@ -0,0 +1,11 @@
+def all_the_same(elements):
+    # your code here
+    return True
+
+if __name__ == '__main__':
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
